64b6e2be41a73f70a4a5de39af7440731fd62e58
Tomas Barton <barton.tomas@gmail.com>
2020-05-11 11:58:02 +0200

    lint

--- spec/classes/catalog_spec.rb
+++ spec/classes/catalog_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe 'pgprobackup::catalog' do
   os, os_facts = on_supported_os.first
-  context "when running with default parameters" do
+  context 'when running with default parameters' do
     let(:facts) { os_facts }
 
     it { is_expected.to compile }
@@ -13,42 +13,40 @@ describe 'pgprobackup::catalog' do
 
     it { is_expected.to contain_user('pgbackup') }
 
-    it { is_expected.to contain_file('/var/lib/pgbackup').with({
-      ensure: 'directory',
-      owner: 'pgbackup',
-      group: 'pgbackup',
-      mode: '0750',
-    }) }
+    it {
+      is_expected.to contain_file('/var/lib/pgbackup').with(ensure: 'directory',
+                                                            owner: 'pgbackup',
+                                                            group: 'pgbackup',
+                                                            mode: '0750')
+    }
 
-    it { is_expected.to contain_file('/var/lib/pgbackup/backups').with({
-      ensure: 'directory',
-      owner: 'pgbackup',
-      group: 'pgbackup',
-      mode: '0750',
-    }) }
+    it {
+      is_expected.to contain_file('/var/lib/pgbackup/backups').with(ensure: 'directory',
+                                                                    owner: 'pgbackup',
+                                                                    group: 'pgbackup',
+                                                                    mode: '0750')
+    }
 
-    it { is_expected.to contain_file('/var/lib/pgbackup/wal').with({
-      ensure: 'directory',
-      owner: 'pgbackup',
-      group: 'pgbackup',
-      mode: '0750',
-    }) }
+    it {
+      is_expected.to contain_file('/var/lib/pgbackup/wal').with(ensure: 'directory',
+                                                                owner: 'pgbackup',
+                                                                group: 'pgbackup',
+                                                                mode: '0750')
+    }
 
     case os_facts[:os]['family']
-      when 'Debian'
-        it {
-          is_expected.to contain_package('pg-probackup-12').with(
-            ensure: 'present',
-          )
-        }
-      when 'RedHat'
-        it {
-          is_expected.to contain_package('pg_probackup-12').with(
-            ensure: 'present',
-          )
-        }
+    when 'Debian'
+      it {
+        is_expected.to contain_package('pg-probackup-12').with(
+          ensure: 'present',
+        )
+      }
+    when 'RedHat'
+      it {
+        is_expected.to contain_package('pg_probackup-12').with(
+          ensure: 'present',
+        )
+      }
     end
-
-
   end
 end
--- spec/classes/instance_spec.rb
+++ spec/classes/instance_spec.rb
@@ -7,7 +7,7 @@ describe 'pgprobackup::instance' do
 
   let(:pre_condition) { 'include postgresql::server' }
 
-  context "with default parameters" do
+  context 'with default parameters' do
     let(:facts) { os_facts }
 
     it { is_expected.to compile }
@@ -15,18 +15,18 @@ describe 'pgprobackup::instance' do
     it { is_expected.to contain_class('pgprobackup::repo') }
 
     case os_facts[:os]['family']
-      when 'Debian'
-        it {
-          is_expected.to contain_package('pg-probackup-12').with(
-            ensure: 'present',
-          )
-        }
-      when 'RedHat'
-        it {
-          is_expected.to contain_package('pg_probackup-12').with(
-            ensure: 'present',
-          )
-        }
+    when 'Debian'
+      it {
+        is_expected.to contain_package('pg-probackup-12').with(
+          ensure: 'present',
+        )
+      }
+    when 'RedHat'
+      it {
+        is_expected.to contain_package('pg_probackup-12').with(
+          ensure: 'present',
+        )
+      }
     end
   end
 end
