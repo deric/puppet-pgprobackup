2980cd82cc3d6872ee2ad75e72bcd58ca9e5eb10
Tomas Barton <barton.tomas@gmail.com>
2020-05-16 12:58:16 +0200

    use ecdsa-sha2-nistp256 as default

--- manifests/init.pp
+++ manifests/init.pp
@@ -45,7 +45,7 @@ class pgprobackup(
   Boolean              $manage_pgpass    = true,
   Boolean              $manage_hba       = true,
   Boolean              $manage_cron      = true,
-  String               $host_key_type    = 'ecdsa-sha2-nistp521',
+  String               $host_key_type    = 'ecdsa-sha2-nistp256',
 ) {
 
   contain pgprobackup::repo
