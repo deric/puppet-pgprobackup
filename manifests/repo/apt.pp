870873463d3f9941dfa4d283fdae4af99aac9438
Tomas Barton <barton.tomas@gmail.com>
2024-02-01 10:44:15 +0100

    Install GPG key via archive (#9)

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -1,21 +1,31 @@
 # @api private
-class pgprobackup::repo::apt inherits pgprobackup::repo {
+class pgprobackup::repo::apt (
+  String $baseurl = 'https://repo.postgrespro.ru/pg_probackup/deb/',
+  String $arch    = 'amd64',
+) {
   include apt
 
-  $default_baseurl = 'https://repo.postgrespro.ru/pg_probackup/deb/'
-
-  $_baseurl = pick($pgprobackup::repo::baseurl, $default_baseurl)
+  $_keyring = '/usr/share/keyrings/pg_probackup.gpg'
+  $_tmp_gpg = '/tmp/pg_probackup.gpg'
+  # TODO: Switch to apt::keyring once supported by puppetlabs-apt
+  # see: https://github.com/puppetlabs/puppetlabs-apt/pull/1128
+  archive { $_tmp_gpg:
+    source          => 'https://repo.postgrespro.ru/pg_probackup/keys/GPG-KEY-PG-PROBACKUP',
+    extract         => true,
+    extract_path    => '/usr/share/keyrings',
+    extract_command => 'gpg --dearmor < %s > pg_probackup.gpg',
+    creates         => $_keyring,
+  }
 
   apt::source { 'pgprobackup':
-    location => $_baseurl,
-    release  => $facts['os']['distro']['codename'],
-    repos    => "main-${facts['os']['distro']['codename']}",
-    key      => {
-      id     => '473F44A5E663EE574CE74E1FA78979F6636D717E',
-      source => 'https://repo.postgrespro.ru/pg_probackup/keys/GPG-KEY-PG_PROBACKUP',
-    },
-    include  => {
+    location     => $baseurl,
+    release      => $facts['os']['distro']['codename'],
+    architecture => $arch,
+    repos        => "main-${facts['os']['distro']['codename']}",
+    include      => {
       src => $pgprobackup::repo::src,
     },
+    keyring      => $_keyring,
+    require      => Archive[$_tmp_gpg],
   }
 }
